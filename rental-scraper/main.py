9ae4128b8262d79fd60d0c0496defd1ea2e6ad6b
davidasix <davidandersonsix@gmail.com>
2023-09-14 17:15:18 -0400

    added rentals listings to all_listings array

--- rental-scraper/main.py
+++ rental-scraper/main.py
@@ -2,6 +2,7 @@ from pprint import pprint
 
 import scrapers.kijiji as kijiji
 import scrapers.kwproperty as kwproperty
+import scrapers.rentals as rentals
 
 def main():
     print('Running Main')
@@ -9,9 +10,15 @@ def main():
     print('Scraping Kijiji')
     kijiji_listings = kijiji.main()
     all_listings.extend(kijiji_listings)
+
     print('Scraping KW Property')
     kwproperty_listings = kwproperty.main()
     all_listings.extend(kwproperty_listings)
+
+    print('Scraping rentals.ca')
+    rentals_listings = rentals.main()
+    all_listings.extend(rentals_listings)
+
     
 if __name__ == "__main__":
     main()
