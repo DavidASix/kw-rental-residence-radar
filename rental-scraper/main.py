dc2a5804e6f61bfa24e523fbac94c040688359e0
davidasix <davidandersonsix@gmail.com>
2023-09-14 14:11:36 -0400

    updated to output kijiji listings

--- rental-scraper/main.py
+++ rental-scraper/main.py
@@ -1,8 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+from pprint import pprint
 
+import scrapers.kijiji as kijiji
 def main():
     print('Running Main')
+    listings = kijiji.main()
+    pprint(listings)
     
 if __name__ == "__main__":
     main()
