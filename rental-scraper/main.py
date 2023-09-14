eaf980d1c9b4ddd9f4d1d7cd66d335e0eee3f7a4
davidasix <davidandersonsix@gmail.com>
2023-09-14 15:27:10 -0400

    Reformatted to collect all scripts in single plac

--- rental-scraper/main.py
+++ rental-scraper/main.py
@@ -1,10 +1,17 @@
 from pprint import pprint
 
 import scrapers.kijiji as kijiji
+import scrapers.kwproperty as kwproperty
+
 def main():
     print('Running Main')
-    listings = kijiji.main()
-    pprint(listings)
+    all_listings = []
+    print('Scraping Kijiji')
+    kijiji_listings = kijiji.main()
+    all_listings.extend(kijiji_listings)
+    print('Scraping KW Property')
+    kwproperty_listings = kwproperty.main()
+    all_listings.extend(kwproperty_listings)
     
 if __name__ == "__main__":
     main()
