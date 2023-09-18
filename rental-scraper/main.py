795f6e2e5ade54d4f47fd2002251aff3e29565c3
davidasix <davidandersonsix@gmail.com>
2023-09-18 15:02:21 -0400

    Updated message text with more information

--- rental-scraper/main.py
+++ rental-scraper/main.py
@@ -31,9 +31,13 @@ def main():
     # If new listings were found notify the users via text message:
     if not len(new_listings):
         return
-    print('Sending notification text:')
-    message_text = f"Found a total of {len(new_listings)}. View them here: {home_url}"
-    print(message_text)
+    print('Sending notification text:\n\n')
+    counts = {}
+    for l in new_listings:
+        counts[l['source']] = counts.get(l['source'], 0) + 1
+    count_str = '\n'.join([f'{key}: {value}' for key, value in counts.items() if value >= 1])
+    message_text = f"🏠 New listing alert! 🏠\n{len(new_listings)} new listings are available\n{count_str}\nView them here: {home_url}"
+    print(message_text, '\n\n')
 
     twilio.send_text(message_text)
     
