2db0d947251a778ad3c7262c02402083b5d55e6e
davidasix <davidandersonsix@gmail.com>
2023-09-18 14:13:53 -0400

    Initialized twilio functions file

---
+++ rental-scraper/twilio_functions.py
@@ -0,0 +1,24 @@
+import os
+import json
+from twilio.rest import Client
+
+base_path = os.path.dirname(__file__)
+secret_path = os.path.join(base_path, './private/twilio.json')
+
+with open(secret_path, "r") as f:
+    # Parse the JSON data into a Python variable
+    secrets = json.load(f)
+    account_sid = secrets['accountSid']
+    auth_token  = secrets['authToken']
+    from_number = secrets['phoneNumber']
+    to_numbers =  secrets['to']
+
+def send_text():
+    print('Sending text')
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to=to_numbers, 
+        from_=from_number,
+        body="Hello from Python!")
+    print('Text sent')
+    print(message.sid)
