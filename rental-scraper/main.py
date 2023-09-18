eef4d0160ed20aa174824ce22cd1b5ef7a660141
davidasix <davidandersonsix@gmail.com>
2023-09-18 14:26:39 -0400

    Added comments and cleaned up code

--- rental-scraper/main.py
+++ rental-scraper/main.py
@@ -11,20 +11,23 @@ home_url = 'https://www.test.com'
 def main():
     print('Running Main')
     all_listings = []
+    # Scrape the sources and get the available new listings.
+
     print('Scraping Kijiji')
-    #kijiji_listings = kijiji.main()
-    #all_listings.extend(kijiji_listings)
+    kijiji_listings = kijiji.main()
+    all_listings.extend(kijiji_listings)
 
     print('Scraping KW Property')
     kwproperty_listings = kwproperty.main()
     all_listings.extend(kwproperty_listings)
 
     print('Scraping rentals.ca')
-    #rentals_listings = rentals.main()
-    #all_listings.extend(rentals_listings)
+    rentals_listings = rentals.main()
+    all_listings.extend(rentals_listings)
 
     new_listings = firebase.import_new_listings(all_listings)
-
+    print(len(new_listings), 'new listings found.')
+    
     # If new listings were found notify the users via text message:
     if not len(new_listings):
         return
@@ -33,7 +36,6 @@ def main():
     print(message_text)
 
     twilio.send_text(message_text)
-
     
 if __name__ == "__main__":
     main()
