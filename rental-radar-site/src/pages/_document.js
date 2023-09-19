0460968406878113bf98d7eea81721a2ca35e377
davidasix <davidandersonsix@gmail.com>
2023-09-18 20:57:09 -0400

    Added new font faces and updated styles

--- rental-radar-site/src/pages/_document.js
+++ rental-radar-site/src/pages/_document.js
@@ -5,8 +5,7 @@ export default function Document() {
     <Html lang="en" data-theme="dark">
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Major+Mono+Display&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/baron-neue" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Martel+Sans:wght@400;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
