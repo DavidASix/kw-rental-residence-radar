3e58a5645fbf7c015e21ef1e1f4e6363cd64286d
davidasix <davidandersonsix@gmail.com>
2023-10-21 17:17:12 -0400

    Added checkbox picker for which house types to hear about

--- rental-radar-site/src/pages/settings.js
+++ rental-radar-site/src/pages/settings.js
@@ -6,10 +6,17 @@ import NavigationLayout from 'src/components/NavigationLayout/';
 import Loader from 'src/components/Loader/';
 import { auth } from 'src/components/Firebase';
 
-export default function Login(props) {
+const houseTypes = {
+	detached: 'Detached', 
+	multi: 'Multi-Unit', 
+	townhouse: 'Townhouse',
+};
+
+export default function Login() {
 	const router = useRouter();
   const [uid, setUid] = useState(null)
 	const [loading, setLoading] = useState(true);
+	const [houseTypeBools, setHouseTypeBools] = useState(Object.fromEntries(Object.keys(houseTypes).map(key => [key, false])))
 
   useEffect(()=>{
     onAuthStateChanged(auth, (user) => {
@@ -28,18 +35,40 @@ export default function Login(props) {
       });   
   }, []);
 
+	const houseButtonClick = (house) => {
+		setHouseTypeBools({...houseTypeBools, [house]: !houseTypeBools?.[house]})
+	}
+
 	const loadingSwitch = () => {
 		if (loading) {
 			return <Loader color='var(--primary)' size={100} center />;
 		}
 		return (
 		<section className={`w-100 row justify-content-center align-content-center p-0 m-0`}>
-			<div className='row col-lg-3 col-12 p-0 py-2'>
-				<span>options</span>
+			<div className='row col-lg-10 col-12 p-0 py-2'>
+				<div className={`row w-100 border`}>
+					<span className='headerFont h5 text-center mb-4'>
+						What type of houses are you interested in?
+					</span>
+					{Object.keys(houseTypes).map((house, i) => (
+						<div key={i} className={`col-lg-4 col-12 px-5 row`}>
+							<input 
+								type="checkbox" 
+								className="btn-check" 
+								id={`${house}_btn`}
+								checked={houseTypeBools?.[house]}
+								onChange={() => houseButtonClick(house)} />
+							<label className="btn btn-outline-primary" htmlFor={`${house}_btn`}>
+								{houseTypes[house]}
+							</label>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 		);
 	}
+	
   return (
     <>
       <Head>
