5813b06d3cfbab7ec76ca6c290c218e0d7e23aad
davidasix <davidandersonsix@gmail.com>
2023-10-21 17:42:13 -0400

    Added switch for sms enabling and disabling

--- rental-radar-site/src/pages/settings.js
+++ rental-radar-site/src/pages/settings.js
@@ -16,7 +16,8 @@ export default function Login() {
 	const router = useRouter();
   const [uid, setUid] = useState(null)
 	const [loading, setLoading] = useState(true);
-	const [houseTypeBools, setHouseTypeBools] = useState(Object.fromEntries(Object.keys(houseTypes).map(key => [key, false])))
+	const [houseTypeBools, setHouseTypeBools] = useState(Object.fromEntries(Object.keys(houseTypes).map(key => [key, false])));
+	const [smsEnabled, setSmsEnabled] = useState(false);
 
   useEffect(()=>{
     onAuthStateChanged(auth, (user) => {
@@ -24,6 +25,7 @@ export default function Login() {
           const uid = user.uid;
           console.log("uid", uid)
           setUid(uid)
+					// TODO: Write a "Set user state" function to get the state from the server and set initial state
 					setLoading(false)
         } else {
           // User is signed out
@@ -32,11 +34,14 @@ export default function Login() {
 					setLoading(true)
 					router.push('/')
         }
-      });   
+      });
   }, []);
 
 	const houseButtonClick = (house) => {
 		setHouseTypeBools({...houseTypeBools, [house]: !houseTypeBools?.[house]})
+	}
+	const enableSmsClick = () => {
+		setSmsEnabled(!smsEnabled);
 	}
 
 	const loadingSwitch = () => {
@@ -46,12 +51,28 @@ export default function Login() {
 		return (
 		<section className={`w-100 row justify-content-center align-content-center p-0 m-0`}>
 			<div className='row col-lg-10 col-12 p-0 py-2'>
-				<div className={`row w-100 border`}>
-					<span className='headerFont h5 text-center mb-4'>
+				<div className={`row w-100 border-top py-5`}>
+					<span className='headerFont h3 text-start mb-5'>
+						Setup SMS Notification Settings
+					</span>
+					<div className={`col-lg-4 col-8 row m-0 p-0`}>
+						<span className='text-start h5 m-0'>Enable SMS Notifications:</span>
+					</div>
+					<div className="form-check form-switch d-flex justify-content-start m-0 col-lg-8 col-4">
+						<input 
+							title='SMS Toggle' 
+							className="form-check-input my-0" 
+							type="checkbox" 
+							role="switch" 
+							onChange={enableSmsClick}
+							checked={smsEnabled}
+							style={{height: 30, width: 75}} />
+					</div>
+					<span className='h5 text-start mt-5'>
 						What type of houses are you interested in?
 					</span>
 					{Object.keys(houseTypes).map((house, i) => (
-						<div key={i} className={`col-lg-4 col-12 px-5 row`}>
+						<div key={i} className={`col-lg-4 col-12 px-5 mt-3 row`}>
 							<input 
 								type="checkbox" 
 								className="btn-check" 
