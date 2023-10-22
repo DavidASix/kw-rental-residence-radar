2219591d10c1ea9cae19a3e1d86b5d58d95a51d8
davidasix <davidandersonsix@gmail.com>
2023-10-22 12:47:19 -0400

    Updated styling to ensure header is drawn above body so nav is always clickable

--- rental-radar-site/src/components/NavigationLayout/index.js
+++ rental-radar-site/src/components/NavigationLayout/index.js
@@ -4,16 +4,18 @@ import Footer from 'src/components/Footer/';
 
 export default function NavigationLayout({ children }) {
   return (
-    <>
-      <header>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", overflowX: 'hidden' }}>
+      <header style={{zIndex: 10}}>
         <NavBar />
       </header>
-      <main className={`d-flex flex-column p-0 justify-content-center align-items-center`}>
+      <main 
+        className={`d-flex flex-column p-0 justify-content-start align-items-center`}
+        style={{flex: 1, zIndex: 1 }}>
         {children}
       </main>
       <footer>
         <Footer />
       </footer>
-    </>
+    </div>
   )
 }
