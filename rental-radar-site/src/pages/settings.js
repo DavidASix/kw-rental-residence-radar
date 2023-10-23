c1e5cc2f276092c0e3370719307b1c68e2305864
davidasix <davidandersonsix@gmail.com>
2023-10-22 21:18:54 -0400

    Fixed styling to remove visual error on mobile view

--- rental-radar-site/src/pages/settings.js
+++ rental-radar-site/src/pages/settings.js
@@ -125,44 +125,44 @@ export default function Login() {
 		}
 		return (
 		<section className={`w-100 row justify-content-center align-content-center p-0 m-0`}>
-			<div className='row col-lg-10 col-12 p-0 py-2'>
-				<div className={`row w-100 border-top py-5`}>
-					<span className='headerFont h3 text-start mb-5'>
-						Setup SMS Notification Settings
-					</span>
-					<div className={`col-lg-4 col-8 row m-0 p-0`}>
-						<span className='text-start h5 m-0'>Enable SMS Notifications:</span>
-					</div>
-					<div className="form-check form-switch d-flex justify-content-start m-0 col-lg-8 col-4">
+			<div className='row col-lg-9 col-12 p-0 py-2'>
+				<span className='headerFont h3 text-start mb-5 col-12'>
+					Setup SMS Notification Settings
+				</span>
+				<div className={`col-lg-4 col-8 row m-0 p-0`}>
+					<span className='text-start h5 m-0'>Enable SMS Notifications:</span>
+				</div>
+				<div className="form-check form-switch d-flex justify-content-start m-0 col-lg-8 col-4">
+					<input 
+						title='SMS Toggle' 
+						className="form-check-input my-0" 
+						type="checkbox" 
+						role="switch" 
+						onChange={enableSmsClick}
+						checked={smsEnabled}
+						style={{height: 30, width: 75}} />
+				</div>
+				<span className='col-12 h5 text-start mt-5'>
+					What type of houses are you interested in?
+				</span>
+				{Object.keys(houseTypes).map((house, i) => (
+					<div key={i} className={`col-lg-4 col-12 row justify-content-center px-5 py-2 m-0`}>
 						<input 
-							title='SMS Toggle' 
-							className="form-check-input my-0" 
 							type="checkbox" 
-							role="switch" 
-							onChange={enableSmsClick}
-							checked={smsEnabled}
-							style={{height: 30, width: 75}} />
+							className="btn-check" 
+							id={`${house}_btn`}
+							checked={houseTypeBools?.[house]}
+							onChange={() => houseButtonClick(house)} />
+						<label className="btn btn-outline-primary" htmlFor={`${house}_btn`}>
+							{houseTypes[house]}
+						</label>
 					</div>
-					<span className='h5 text-start mt-5'>
-						What type of houses are you interested in?
-					</span>
-					{Object.keys(houseTypes).map((house, i) => (
-						<div key={i} className={`col-lg-4 col-12 px-5 mt-3 row`}>
-							<input 
-								type="checkbox" 
-								className="btn-check" 
-								id={`${house}_btn`}
-								checked={houseTypeBools?.[house]}
-								onChange={() => houseButtonClick(house)} />
-							<label className="btn btn-outline-primary" htmlFor={`${house}_btn`}>
-								{houseTypes[house]}
-							</label>
-						</div>
-					))}
-					<span className='h5 text-start mt-5'>
-						What's your maximum rent price?
-					</span>
-					<span className='headerFont text-end'>${rentMaxPrice}.00/month</span>
+				))}
+				<span className='col-12 h5 text-start mt-5'>
+					What's your maximum rent price?
+				</span>
+				<span className='col-12 headerFont text-end'>${rentMaxPrice}.00/month</span>
+				<div className={`col-12 row justify-content-center px-lg-5 px-2 py-2 m-0`}>
 					<input 
 						type="range" 
 						className="form-range" 
@@ -171,10 +171,10 @@ export default function Login() {
 						step="100" 
 						value={rentMaxPrice} 
 						onChange={rentRangeOnChange} />
-					<div className='d-flex flex-row justify-content-between'>
-						<small>$100</small>
-						<small>$10,000</small>
-					</div>
+				</div>
+				<div className='d-flex flex-row justify-content-between px-lg-5 px-2'>
+					<small>$100</small>
+					<small>$10,000</small>
 				</div>
 			</div>
 		</section>
