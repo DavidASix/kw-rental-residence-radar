8c7d89512f349887c8d4fb47e85aa00ede73fb80
davidasix <davidandersonsix@gmail.com>
2023-12-04 14:50:08 -0500

    Completed hero section layout, pushing to desktop

--- rental-radar-site/src/pages/index.js
+++ rental-radar-site/src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+
+import TopSeperator from 'public/images/seperator-top.svg';
 import s from "src/pages/home.module.css";
 import cs from "src/styles/common.module.css";
 
@@ -11,16 +13,49 @@ export default function Home(props) {
       <Head>
       </Head>
       <NavigationLayout>
-        <section 
-          className={`py-md-5 py-3 w-100 row position-relative justify-content-center align-content-center text-center`}
-          style={{zIndex: 0}}>
-          <h1 className='display-2' style={{zIndex: 10}}>
-            KW Rental Radar
-          </h1>
-          <p className='h4 mt-4' style={{zIndex: 10}}>
-            Finding a house in the KW is difficult. Make it easier with rental radar.
-          </p>
+        <section className={`${s.hero} gradient-light col-12 row justify-content-end border border-info`}
+            style={{ overflowX: 'clip', overflowY: 'visible', zIndex: 10}}>
+          <div className={`col-5 row justify-content-center align-content-center border`}>
+            <h1 className='display-6 header' style={{zIndex: 10}}>
+              Waterloos rental competition is <span className='heavy'>fierce</span>
+            </h1>
+            <p className='h4 ps-3'>
+              With a rental house vacancy rate of 1.1%* in 2022, you’ll need a edge to find a house
+            </p>
+          </div>
+          <div className='col-6 d-flex justify-content-center align-items-center position-relative'>
+              <img 
+                src="/images/house.png" 
+                className="position-absolute start-0" 
+                style={{height: 'auto', width: '100%', minWidth: 600, maxHeight: '90vh', transform: 'scalex(-1)scale(1.175)' }}
+                alt="3D render of a house" />
+          </div>
         </section>
+
+        <section className={`${s.issueOutline} col-12 position-relative row justify-content-center gradient-accent-muted`}
+          style={{zIndex: 5}}>
+          <div className='w-100 position-absolute top-0' style={{height: '5vh', backgroundColor: 'var(--modal)'}} />
+          <TopSeperator 
+            style={{position: 'absolute', top:'5vh', fill: 'var(--modal)', padding: 0 }} />
+          <div className='col-8 row'>
+          <div className={`col-4 p-2 border`}>
+              <div className={`w-100 h-100 ${cs.frosted} row rounded-4`}>
+                <h2>Text</h2>
+              </div>
+            </div>
+            <div className={`col-4 p-2 border`}>
+              <div className={`w-100 h-100 ${cs.frosted} row rounded-4`}>
+                <h2>Text</h2>
+              </div>
+            </div>
+            <div className={`col-4 p-2 border`}>
+              <div className={`w-100 h-100 ${cs.frosted} row rounded-4`}>
+                <h2>Text</h2>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </NavigationLayout>
     </>
   );
